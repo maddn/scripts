35cbfb19f07b09c89ffd45e5b98ac1ca7bf2ef7c
Michael Maddern <michael.maddern@gmail.com>
2015-05-21 14:51:50 +0100

    Modified to backup to /mnt/backup. Added Xresource XFCE4 conversion script.

--- rsync-backup.sh
+++ rsync-backup.sh
@@ -1,24 +1,24 @@
 #/bin/bash
 
 echo "*** Backing up Personal folder ***"
-rsync -rtv --modify-window=1 --delete --exclude='.localized' --exclude='.DS_Store' --exclude='$RECYCLE.BIN/' ~/Personal /Volumes/Backup
+rsync -rtv --modify-window=1 --delete --exclude='.localized' --exclude='.DS_Store' --exclude='$RECYCLE.BIN/' ~/Personal /mnt/backup
 echo
 echo "*** Backing up Work folder ***"
-rsync -rtv --modify-window=1 --delete --exclude='.localized' --exclude='.DS_Store' --exclude='$RECYCLE.BIN/' ~/Work /Volumes/Backup
+rsync -rtv --modify-window=1 --delete --exclude='.localized' --exclude='.DS_Store' --exclude='$RECYCLE.BIN/' ~/Work /mnt/backup
 echo
 echo "*** Backing up Pictures folder ***"
-rsync -rtv --modify-window=1 --delete --exclude='.localized' --exclude='.DS_Store' --exclude='$RECYCLE.BIN/' ~/Pictures /Volumes/Backup
+rsync -rtv --modify-window=1 --delete --exclude='.localized' --exclude='.DS_Store' --exclude='$RECYCLE.BIN/' ~/Pictures /mnt/backup
 echo
 echo "*** Backing up Software folder ***"
-rsync -rtv --modify-window=1 --delete --exclude='.localized' --exclude='.DS_Store' --exclude='$RECYCLE.BIN/' ~/Software /Volumes/Backup
+rsync -rtv --modify-window=1 --delete --exclude='.localized' --exclude='.DS_Store' --exclude='$RECYCLE.BIN/' ~/Software /mnt/backup
 echo
 echo "*** Backing up Music folder ***"
-rsync -rtv --modify-window=1 --delete --exclude='.localized' --exclude='.DS_Store' --exclude='$RECYCLE.BIN/' ~/Music /Volumes/Backup
+rsync -rtv --modify-window=1 --delete --exclude='.localized' --exclude='.DS_Store' --exclude='$RECYCLE.BIN/' ~/Music /mnt/backup
 echo
 echo "*** Backing up Movies folder ***"
-rsync -rtv --modify-window=1 --delete --exclude='.localized' --exclude='.DS_Store' --exclude='$RECYCLE.BIN/' ~/Movies /Volumes/Backup
+rsync -rtv --modify-window=1 --exclude='.localized' --exclude='.DS_Store' --exclude='$RECYCLE.BIN/' ~/Movies /mnt/backup
 echo
 echo "*** Backing up Old-Stuff folder ***"
-rsync -rtv --modify-window=1 --delete --exclude='.localized' --exclude='.DS_Store' --exclude='$RECYCLE.BIN/' ~/Old-Stuff /Volumes/Backup
+rsync -rtv --modify-window=1 --delete --exclude='.localized' --exclude='.DS_Store' --exclude='$RECYCLE.BIN/' ~/Old-Stuff /mnt/backup
 echo
 
